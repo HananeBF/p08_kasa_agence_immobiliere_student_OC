b8fd6b220b39aa8e336ed76c5595d32b900583d2
HananeBF <hanane.benjeddir@gmail.com>
2024-10-24 14:25:52 +0200

    add modification on home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import React from 'react';
     return (
         <div className='Home'>
             Home avec (le Layout) une grande image avec un texte dedans,  et plein de composants logements avec des liens qui amène à des pages de fiches logement, et enfin le footer. Penser au onClick pour les logements pour que ce soit dynamique
+            
         </div>
     )
 }
