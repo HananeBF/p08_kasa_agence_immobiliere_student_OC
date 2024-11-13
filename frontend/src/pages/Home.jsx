a03ccb1261123b1a37eb8de59e0d021645c2ccaa
HananeBF <hanane.benjeddir@gmail.com>
2024-11-13 11:44:35 +0100

    restructure css for home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { logementService } from'@/_services/logement.service.jsx'
-import '@/App.css'
+import '@pages/Home.css'
+import Banner from '@/assets/HomeBanner.png'
 
 const Home = () => {
     
@@ -25,22 +26,22 @@ const Home = () => {
         
     }, [])
 
-    // const logementA = (id) => {
-    //     console.log('click')
-    //     navigate("./fiche-logement/"+id)
-    // }
 
     return (
         <div className='Home'>
-            Home une grande image avec un texte dedans,  et plein de composants logements avec des liens qui amène à des pages de fiches logement,. Penser au onClick pour les logements pour que ce soit dynamique
+            <div>Home une grande image avec un texte dedans,  et plein de composants logements avec des liens qui amène à des pages de fiches logement,. Penser au onClick pour les logements pour que ce soit dynamique</div>
+            <div className='Banner'>
+                <img src={Banner}></img>
+            </div>
             <div className='galleryLocations'>
                 <div className="CardFlat">
                     {
                         properties.map(flats => (
                         <Link to={`/fiche-logement/${flats.id}`} key={flats.id} className='Flat'>
-                            <img src={flats.cover} alt={flats.title}/>
-                            <p>{flats.title}</p>
-                           
+                            <div className='imgLocation'>
+                                <img src={flats.cover} alt={flats.title}/>
+                                <span>{flats.title}</span>
+                            </div>
                              
                         </Link>
 
